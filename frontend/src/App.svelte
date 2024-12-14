556355dbbac7e90f0c9da2d69c054ec3c3a62102
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2024-12-15 00:48:29 +0100

    Zkousim ze Svelte jestli funguje Flask API

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,19 @@
+<script>
+  let organisationName = "";
+  fetch("./api")
+  .then(d => d.json())
+  .then(d => {
+    organisationName = d.organization;
+  });
+</script>
+
 <main>
   <p>
   Hello TdA
   </p>
+  <p>Another element</p>
+
+  
+  <h1>Organizace se jmenuje {organisationName}</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, porro repellat dignissimos et corporis quibusdam veritatis quam qui provident ad. Totam quis quia eum libero temporibus dolores. Obcaecati, deleniti placeat?</p>
 </main>
