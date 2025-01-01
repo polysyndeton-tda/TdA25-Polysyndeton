ed392845bc9d07fda4a1f8afaae4164f514af531
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-01-01 20:22:12 +0100

    Add checkVictory function

--- frontend/src/Board.svelte
+++ frontend/src/Board.svelte
@@ -30,9 +30,76 @@
         }
     }
 
+    function checkVictory(rowIndex, columnIndex) {
+        /*Checks if latest move on rowIndex, columnIndex is a winning one
+        (made to be called from handleMove)
+        */
+
+        // board[0,0] is the north-west corner
+        let matchedInDirection = {'n': 0, 'ne': 0, 'e': 0, 'se': 0, 's': 0, 'sw': 0, 'w': 0, 'nw': 0};
+
+        //Checks in which directions out of directionsToLookAt, there are squares of the player who played this move
+        //With every iteration, the directionsToLookAt for the next iteration
+        //stays the same or gets smaller, depending on if there are relevant squares in that distance from (rowIndex, columnIndex)
+        //All relevant square matches are put into matchedInDirection
+        //To evaluate victory, values in opposite directions are added up
+        //For victory of 5 items in a row, 4 items need to be found around
+        let directionsToLookAt = new Set(['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw']);
+        for(let dist = 1; dist <= 4; dist++){
+            //explicit bounds checking is not necessary in JS (if out of bounds it is undefined)
+            let allPossiblePositions = {
+                "n":    [rowIndex - dist, columnIndex, "n"],
+                "ne":   [rowIndex - dist, columnIndex + dist, "ne"],
+                "e":    [rowIndex, columnIndex + dist, "e"],
+                "se":   [rowIndex + dist, columnIndex + dist, "se"],
+                "s":    [rowIndex + dist, columnIndex, "s"],
+                "sw":   [rowIndex + dist, columnIndex - dist, "sw"],
+                "w":    [rowIndex, columnIndex - dist, "w"],
+                "nw":   [rowIndex - dist, columnIndex -dist, "nw"]
+            };
+            //.keys() returns an iterator, which should have worked (and be lazily evaluated?) => for now a destructuring operator works
+            let squaresInDirectionsToLookAt = [...directionsToLookAt.keys()].map(e => {
+                let [x,y, direction] = allPossiblePositions[e];
+                console.log(x,y,direction)
+                return [boardApiInfo.board[x][y], direction];
+            });
+            console.log("l", directionsToLookAt.size)
+            console.log("squaresInDirectionsToLookAt", squaresInDirectionsToLookAt);
+            //squares of the same player who made this move
+            let relevantSquares = squaresInDirectionsToLookAt.filter(e => {
+                let [value, direction] = e;
+                console.log(value, direction)
+                if(value == naTahu) return true;
+            });
+            console.log("value, direction where correct", Array.from(relevantSquares));
+            let relevantDirections = relevantSquares.map(e=> e[1]);
+            console.log("directions where to search", relevantDirections);
+
+            directionsToLookAt = new Set(relevantDirections);
+
+            Array.from(relevantDirections).forEach((value)=> {
+                matchedInDirection[value]++
+            });
+        }
+        console.log("result", Object.entries(matchedInDirection));
+        let lengths = [
+            matchedInDirection["s"] + matchedInDirection["n"],
+            matchedInDirection["ne"] + matchedInDirection["sw"],
+            matchedInDirection["e"] + matchedInDirection["w"],
+            matchedInDirection["se"] + matchedInDirection["nw"]
+        ];
+        console.log("is victory", lengths.some(v => v >= 4)); //vyhra muze byt >= 5 v rade (tedy jsme vedle tohoto tahu nasli alespon 4 pole s nim v lajne)
+        return lengths.some(v => v == 4);
+    }
+
+    
     function handleMove(rowIndex, columnIndex) {
         boardApiInfo.board[rowIndex][columnIndex] = naTahu;
         lastMoved = naTahu;
+
+        if (checkVictory(rowIndex, columnIndex, naTahu)) {
+            console.log(`${naTahu} wins!`);
+        }
         if (lastMoved === "X") {
             naTahu = "O";
         } else {
