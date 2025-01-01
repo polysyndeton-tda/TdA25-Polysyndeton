5ae6bdd8b74d7c012650c9f169d13d08afb3130d
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-01-01 16:09:24 +0100

    Fix the start new game button

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -11,21 +11,26 @@
       => tam proste staci naservovat taky tuto App.svelte stránku (co bude na /game), s parametrem uuid si poradim a rovnou nactu tu hru
 -->
 {#if !beginNewGame && !gameInfo.selected}
-<h2>Uložené hry</h2>
-<GamePicker></GamePicker>
-<button onclick={()=> beginNewGame = true}>Začít novou hru</button>
-{:else}
-<!-- <Board boardApiInfo={gameInfo.apiResponse}></Board> -->
+  <h2>Uložené hry</h2>
+  <GamePicker></GamePicker>
+  <button onclick={()=> {
+    beginNewGame = true;
+    gameInfo.apiResponse = {};
+    gameInfo.apiResponse.board = Array(15).fill().map(() => Array(15).fill("")); //2D array of "" 15x15
+    gameInfo.selected = true;
+    }}>Začít novou hru
+  </button>
 {/if}
 
 
 {#if gameInfo.selected}
-<button onclick={()=>{
-  gameInfo.selected = false;
-  beginNewGame = false;
-}}>Back
-</button>
-
-<p>Selected game {gameInfo.apiResponse.uuid}</p>
-<Board boardApiInfo={gameInfo.apiResponse}></Board>
+  <button onclick={()=>{
+    gameInfo.selected = false;
+    beginNewGame = false;
+  }}>Back
+  </button>
+  {#if gameInfo.apiResponse.uuid}
+    <p>Selected game {gameInfo.apiResponse.uuid}</p>
+  {/if}
+  <Board boardApiInfo={gameInfo.apiResponse}></Board>
 {/if}
