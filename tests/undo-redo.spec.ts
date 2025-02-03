c1f4633a9c06fbbe09c5c3f2af555f8307573a32
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-02-03 21:40:03 +0100

    More info about the Webkit crash on the undo redo test 1 (commit 4fbd5c9bcf1ea02b0dd3963292bd4dbe88fe0bdb)

--- tests/undo-redo.spec.ts
+++ tests/undo-redo.spec.ts
@@ -4,9 +4,28 @@ test('Undo redo 1', async ({ page }) => {
   await page.goto('http://127.0.0.1:5000/game');
   //making any three moves on the board
   await page.locator('.row:nth-child(8) > .field:nth-child(5)').click();
-  await expect(page.locator('.row:nth-child(8) > .field:nth-child(5)')).toHaveClass(/X/);
-  // await page.locator('.row:nth-child(9) > .field:nth-child(5)').click();
-  // await page.locator('.row:nth-child(10) > .field:nth-child(5)').click();
+
+  /*fails in Webkit (Playwright, Safari) with "" 
+  Real life workaround: go to http://127.0.0.1:5000 first, from there click on "Nová hra" (instead of going to this page directly)
+  Locator: locator('.row:nth-child(8) > .field:nth-child(5)')
+  Expected pattern: /X/
+  Received string:  ""
+
+  When this, the line in question, gets commented out:
+  */
+  // await expect(page.locator('.row:nth-child(8) > .field:nth-child(5)')).toHaveClass(/X/);
+  //and there is another .click() on the game square, it gets:
+  //   Error: locator.click: Target page, context or browser has been closed
+  // Call log:
+  //   - waiting for locator('.row:nth-child(9) > .field:nth-child(5)')
+
+  //Again, only on Webkit
+
+  //The real-life analogy of this test, going to http://127.0.0.1:5000/game in Epiphany 46, and clicking the field **once** freezes it
+  //Other browsers are fine
+
+  await page.locator('.row:nth-child(9) > .field:nth-child(5)').click();
+  await page.locator('.row:nth-child(10) > .field:nth-child(5)').click();
   // //undoing the third and second move
   // await page.getByRole('button', { name: 'Undo' }).click();
   // await page.getByRole('button', { name: 'Undo' }).click();
