f3dad41d6f19888884d66486e5f8f67cec1baf0e
Luka Králík <luka.kralik@gmail.com>
2025-02-08 20:47:55 +0100

    user model

--- backend/src/models.py
+++ backend/src/models.py
@@ -17,3 +17,16 @@ class Game(db.Model):
     board = db.Column(db.Text)
     width = db.Column(db.Integer)  # board[0] len
     heigth = db.Column(db.Integer)  # board len
+
+class User(db.Model):
+    uuid = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(
+        db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
+
+    username = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False)
+
+    elo = db.Column(db.Integer, nullable=False, default=400)
+    wins = db.Column(db.Integer, nullable=False, default=0)
+    draws = db.Column(db.Integer, nullable=False, default=0)
+    losses = db.Column(db.Integer, nullable=False, default=0)
