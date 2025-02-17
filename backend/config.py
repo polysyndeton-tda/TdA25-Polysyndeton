adfa0bb7f2954566f4b490ebcfe7f7a4f8214e2b
Luka Králík <luka.kralik@gmail.com>
2025-02-17 11:14:00 +0100

    Add logging to config

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,20 @@
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv(override=True)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+logger = logging.getLogger(__name__)
 
 
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_AS_ASCII = False
-    JWT_SECRET_KEY = os.getenv("SECRET_KEY") or os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.environ["SECRET_KEY"] or os.environ["JWT_SECRET_KEY"]
+
+    logger.info("Listing all environment variables:")
+    for key, value in os.environ.items():
+        logger.info(f"{key}: {value}")
--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -2,7 +2,6 @@ import logging
 import os
 
 from flask import Flask
-from logging.handlers import RotatingFileHandler
 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -25,20 +24,3 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from src import routes
-
-if not app.debug:
-    if not os.path.exists("logs"):
-        os.mkdir("logs")
-    file_handler = RotatingFileHandler(
-        "logs/tictactoe.log", maxBytes=10240, backupCount=10
-    )
-    file_handler.setFormatter(
-        logging.Formatter(
-            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
-        )
-    )
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-
-    app.logger.setLevel(logging.INFO)
-    app.logger.info("Tda25-Polysyndeton")
