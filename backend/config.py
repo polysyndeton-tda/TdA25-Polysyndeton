618a0c2413fdadeb670ecb6620af999009bcf665
Luka Králík <luka.kralik@gmail.com>
2025-02-18 13:37:29 +0100

    (Auth) hardcode superuser details

--- backend/config.py
+++ backend/config.py
@@ -13,7 +13,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_AS_ASCII = False
-    JWT_SECRET_KEY = os.getenv("SECRET_KEY") or os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY") or "secret-key"
+    SECRET_KEY = os.getenv("SECRET_KEY") or "secret-key"
+    SUPERUSER_EMAIL = os.getenv("SUPERUSER_EMAIL") or "tda@scg.cz"
+    SUPERUSER_USERNAME = os.getenv("SUPERUSER_USERNAME") or "TdA"
+    SUPERUSER_PASSWORD = os.getenv("SUPERUSER_PASSWORD") or "StudentCyberGames25!"
 
     logger.info("Listing all environment variables:")
     for key, value in os.environ.items():
