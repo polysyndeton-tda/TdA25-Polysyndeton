ea3c6b49f6ddcf9a30bd87d7fe20104199dd4639
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-02-18 15:39:35 +0100

    Table on admin page (proof of concept, better coming)

--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -1,17 +1,51 @@
 <script>
     import Login from "$lib/Login.svelte";
+    import { User, getUsers } from "$lib/shared.svelte.js";
+    import { onMount } from "svelte";
 
+    let users = $state();
+    let loaded = $state(false);
+
+    async function load(){
+        users = await getUsers();
+        console.log("users", users);
+        loaded = true;
+    }
+
+    onMount(load);
 </script>
-<div class="centerDiv">
 
-</div>
 
 <div class="centerBox">
-    <h1>Admin</h1>
-    <Login floating={false} mode="login"/>
+    <h1>Admin</h1>    
+
+    {#if User.loggedIn}
+        {#if loaded}
+        <table>
+            <tbody> 
+                <tr>
+                    <th>Vytvořen:</th>
+                    <th>Jméno:</th>
+                    <th>Email:</th>
+                    <th>ELO:</th>
+                    <th>Výhry:</th>
+                </tr>
+                {#each users as user}
+                    <tr>
+                        <td>{user.createdAt}</td>
+                        <td>{user.username}</td>
+                        <td>{user.email}</td>
+                        <td>{user.elo}</td>
+                        <td>{user.wins}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+        {/if}
+    {:else}
+        <Login floating={false} mode="login"/>
+    {/if}
 </div>
-
-
 <style>
    .centerBox{
     display: flex;
