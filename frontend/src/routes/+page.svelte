c15af37a43b18245ed34ef995c76e846d13778b8
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-02-21 14:03:33 +0100

    Remove unused CSS (the scopedPreflightStyles plugin takes care of that)

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,4 @@
-<div class="revert-tailwind">
+<div>
     <h1>Home</h1>
     <a href="/admin">Admin</a>
 </div>
-
-
-<style>
-    .revert-tailwind{
-        all: initial;
-    }
-
-    .revert-tailwind > * {
-        all: revert;
-    }
-</style>
