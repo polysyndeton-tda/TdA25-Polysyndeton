6c78b24b127c1c622dfdfc2251ebae749e6828f8
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-02-22 14:54:35 +0100

    Repurpose build.sh for quick dev builds, offering me source maps for the browser in the browser debugger and live reloading
    For production builds, the `buildRunDocker.sh` remains

--- build.sh
+++ build.sh
@@ -1,9 +1,28 @@
+# Setup trap to kill background processes on script exit 
+#(to ma pry ukoncit npm run dev spusteny na pozadi, ktery by jinak ctrl c neukoncil a pry by bylo potreba pkill -f "npm run dev" 
+trap 'kill $(jobs -p)' EXIT
+
 cd frontend
 npm install
-npm run build
+rm -rf ./build/ #clear the build folder to be sure there are no files from the previous build
+
+# Backup original .env if it exists
+# [ -f .env ] && cp .env .env.backup
+
+# Create temporary .env
+echo "PUBLIC_API_BASE_URL=http://localhost:5000/api/v1" > .env
+
+npm run dev & #& means run in background
 
 echo $(pwd)
+rm -rf ../backend/static/* #same with the backend static folder (there were issues before)
 cp -a ./build/. ../backend/static
+
+#Restore doesn't work - the frontend somehow picks up on it (probably live reload)
+#So instead modify .env here and modify it in buildRunDocker.sh)
+
+# Restore original .env
+# [ -f .env.backup ] && mv .env.backup .env
 
 cd ../backend
 pip install --no-cache-dir -r requirements.txt --break-system-packages
