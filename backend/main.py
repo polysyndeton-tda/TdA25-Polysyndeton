3f74a7b95d251b72822c437d1a10ef705ebdcb1d
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2025-03-14 23:55:35 +0100

    Enable the flask debugger at "/console"  route (requires knowledge of DEBUGGER-PIN)

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,17 @@ from src import app, db, socketio
 from src.models import Game
 import gevent
 from src.routes import matchmaking_loop
+from werkzeug.debug import DebuggedApplication
 
 if __name__ == "__main__":
     print(app.url_map)
 
     gevent.spawn(matchmaking_loop)
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host="0.0.0.0", port=5000)
+
+    #if app.debug is set to true, allow us to see the Flask Debugger through Web UI (requires entering Flask Debugger-PIN)
+    if app.debug:
+        app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
 
 
 @app.shell_context_processor
